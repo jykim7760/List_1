1fbcc0e966435078eea7737a6e4e712e046fcda1
jykim7760 <jykim7760@naver.com>
2020-04-14 19:59:27 +0900

    코드구조정리

--- app/src/main/java/com/practice/test/MainActivity.kt
+++ app/src/main/java/com/practice/test/MainActivity.kt
@@ -25,16 +25,55 @@ class MainActivity : AppCompatActivity() {
         gameList.add(data("리니지2M", "NC Soft", 3))
         gameList.add(data("리니지M", "NC Soft", 4))
         gameList.add(data("AFK 아레나", "Lilith Games", 1))
+        gameList.add(data("아스팔트 8", "GameLoft", 5))
+        gameList.add(data("MineCraft", "Mojang", 4))
+        gameList.add(data("아스팔트 7", "GameLoft", 4))
+        gameList.add(data("팔라독", "FazeCat", 2))
+        gameList.add(data("Plants vs. Zombies", "EA Games", 3))
+        gameList.add(data("스왐피", "Disney", 5))
+        gameList.add(data("리니지2M", "NC Soft", 3))
+        gameList.add(data("리니지M", "NC Soft", 4))
+        gameList.add(data("AFK 아레나", "Lilith Games", 1))
+        gameList.add(data("아스팔트 8", "GameLoft", 5))
+        gameList.add(data("MineCraft", "Mojang", 4))
+        gameList.add(data("아스팔트 7", "GameLoft", 4))
+        gameList.add(data("팔라독", "FazeCat", 2))
+        gameList.add(data("Plants vs. Zombies", "EA Games", 3))
+        gameList.add(data("스왐피", "Disney", 5))
+        gameList.add(data("리니지2M", "NC Soft", 3))
+        gameList.add(data("리니지M", "NC Soft", 4))
+        gameList.add(data("AFK 아레나", "Lilith Games", 1))
+        gameList.add(data("아스팔트 8", "GameLoft", 5))
+        gameList.add(data("MineCraft", "Mojang", 4))
+        gameList.add(data("아스팔트 7", "GameLoft", 4))
+        gameList.add(data("팔라독", "FazeCat", 2))
+        gameList.add(data("Plants vs. Zombies", "EA Games", 3))
+        gameList.add(data("스왐피", "Disney", 5))
+        gameList.add(data("리니지2M", "NC Soft", 3))
+        gameList.add(data("리니지M", "NC Soft", 4))
+        gameList.add(data("AFK 아레나", "Lilith Games", 1))
+        gameList.add(data("아스팔트 8", "GameLoft", 5))
+        gameList.add(data("MineCraft", "Mojang", 4))
+        gameList.add(data("아스팔트 7", "GameLoft", 4))
+        gameList.add(data("팔라독", "FazeCat", 2))
+        gameList.add(data("Plants vs. Zombies", "EA Games", 3))
+        gameList.add(data("스왐피", "Disney", 5))
+        gameList.add(data("리니지2M", "NC Soft", 3))
+        gameList.add(data("리니지M", "NC Soft", 4))
+        gameList.add(data("AFK 아레나", "Lilith Games", 1))
 
 
+        fun setValues(){
 
-        Adapter = Adapter(this, R.layout.app, gameList)
+            adddata()
+
+            Adapter = Adapter(this, R.layout.app, gameList)
         gameListView.adapter = Adapter
 
 
         gameListView.setOnItemClickListener { parent, view, position, id -> }
         Toast.makeText(this, "{position}번 줄 클릭", Toast.LENGTH_SHORT).show()
-
+        }
 
     }
 }
